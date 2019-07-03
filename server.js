c46c5afe6d53db375a2a1315bcae14d3e6639e30
Will <whof@bu.edu>
2019-07-03 15:14:08 -0400

    set up html route, html baseline file

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const db = require("./models")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./routes/htmlroutes")(app)
+
 db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
     });
-});
+}); 
