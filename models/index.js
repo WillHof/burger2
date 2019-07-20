3f2efdb10c2a8f9e76ce860da057d155c9f0ce12
Will <whof@bu.edu>
2019-07-20 13:40:42 -0400

    tried merging roxy's very helpful heroku updates into mine to get heroku to deploy successfully

--- models/index.js
+++ models/index.js
@@ -1,13 +1,12 @@
 'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.JAWSDB_URL || 'development';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-console.log(config)
-const db = {}
-console.log("nothing")
+const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
--- server.js
+++ server.js
@@ -9,15 +9,6 @@ app.use(express.static("public"))
 require("./routes/htmlroutes")(app)
 require("./routes/apiroutes")(app)
 
-db.sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
 db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
